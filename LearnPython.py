fa6fa9389e4217539ca21652694652c276547558
coding102 <mercercountynj@gmail.com>
2022-02-11 18:06:06 -0500

    exercises

--- LearnPython.py
+++ LearnPython.py
@@ -1149,4 +1149,23 @@ some_person = Person("Osborne")
 
 # Call the greeting method
 print(some_person.greeting())
+
+
+# A docstring is a brief text that explains what something does.
+# %%
+class Piglet:
+    """Represents a piglet that can say their name."""
+    years = 0
+    name = ""
+
+    def speak(self):
+        """Outputs a message including the name of the piglet."""
+        print("Oink! I'm {}! Oink!".format(self.name))
+
+    def pig_years(self):
+        """Converts the current age to  equivalent pig years."""  # A docstring is a brief text that explains what something does.
+        return self.years * 18
+    help(pig_years)  # Call docstring
+
+
 # %%
